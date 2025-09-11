0be80080b611f08bc5c4a01820ca6c74a0aa1d73
Chris Hanlon <christopher.hanlon1@gmail.com>
2025-09-11 09:30:28 -0600

    Completed website part, ready to deploy
    Assignment done, ready to deploy to vercel

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,9 +1,11 @@
-function StudentInfo() {
+import Link from "next/link";
+export default function StudentInfo() {
   return (
     <div>
       <h2>Chris Hanovich (formerly Hanlon)</h2>
-      <Link href="
-
+      <Link href="https://github.com/Christopher-Hanovich/cprg306-assignments">
+        GitHub Repo
+      </Link>
     </div>
   );
 }
