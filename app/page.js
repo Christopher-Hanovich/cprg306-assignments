91b9153d50708a3e723e14393b683707176286b0
Chris Hanlon <christopher.hanlon1@gmail.com>
2025-09-25 09:33:32 -0600

    Added Week 3 Assignment

--- app/page.js
+++ app/page.js
@@ -2,8 +2,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2</Link>
+      <h1 className="text-2xl font-bold text-blue-700 mb-4 text-center">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <p className="mb-2 text-center">
+        <Link href="/week-2">Week 2</Link>
+      </p>
+      <p className="mb-2 text-center">
+        <Link href="/week-3">Week 3</Link>
+      </p>
     </main>
   );
 }
