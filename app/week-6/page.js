4f517fbc6c42a501be0fda47e3fa314828aa1ca9
Chris Hanlon <christopher.hanlon1@gmail.com>
2025-10-16 09:53:15 -0600

    accepted changes for week 5 assignment

---
+++ app/week-6/page.js
@@ -0,0 +1,9 @@
+import ItemList from "./item-list";
+export default function page() {
+  return (
+    <main>
+      <h1 className="text-center font-bold text-5xl">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+}
