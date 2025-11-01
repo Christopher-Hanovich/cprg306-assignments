f7714e79ecacaeb6ac5eec872e6970c6dbeddf66
Chris Hanlon <christopher.hanlon1@gmail.com>
2025-10-31 21:45:14 -0600

    Update item.js
    changed text to black for high contrast

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -8,9 +8,9 @@ export default function Item({ item, onSelect }) {
       className="cursor-pointer p-3 border rounded-lg hover:bg-blue-50 transition"
     >
       <div className="flex flex-col">
-        <span className="text-white ">{name}</span>
-        <span className="text-sm text-white">Quantity: {quantity}</span>
-        <span className="text-sm text-white">Category: {category}</span>
+        <span className="text-black ">{name}</span>
+        <span className="text-sm text-black">Quantity: {quantity}</span>
+        <span className="text-sm text-black">Category: {category}</span>
       </div>
     </li>
   );
