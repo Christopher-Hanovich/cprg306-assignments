6c432a2b7328f45fdbf27ee2e276daa3926286ec
AxelArredondo <axel.arredondotapia@edu.sait.ca>
2025-11-30 20:21:49 -0700

    changed week 9 back to original

--- app/week-9/_utils/firebase.js
+++ app/week-9/_utils/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDIBpGlBV8WO2YOfF3UUyDqb8aWloSHqiQ",
-  authDomain: "cprg306-assignments-b0219.firebaseapp.com",
-  projectId: "cprg306-assignments-b0219",
-  storageBucket: "cprg306-assignments-b0219.firebasestorage.app",
-  messagingSenderId: "834940330463",
-  appId: "1:834940330463:web:b72a74f2decc1e5350246a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
